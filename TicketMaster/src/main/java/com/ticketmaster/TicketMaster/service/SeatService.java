d4d77ac26e9a750e06b340b4c0e425d0ab93361b
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-20 13:41:59 +0530

    add seat locking mechanism and SeatService

---
+++ TicketMaster/src/main/java/com/ticketmaster/TicketMaster/service/SeatService.java
@@ -0,0 +1,41 @@
+package com.ticketmaster.TicketMaster.service;
+
+import com.ticketmaster.TicketMaster.entity.Seat;
+import com.ticketmaster.TicketMaster.enums.SeatStatus;
+import com.ticketmaster.TicketMaster.exception.SeatLockedException;
+import com.ticketmaster.TicketMaster.repository.SeatRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
+public class SeatService {
+
+    @Autowired
+    private SeatRepository seatRepo;
+
+    @Transactional
+    public Seat holdSeat(Long seatId, Long userId) {
+
+        Seat seat = seatRepo.findById(seatId)
+                .orElseThrow(() -> new RuntimeException("Seat not found"));
+
+        if (seat.getStatus() == SeatStatus.HELD &&
+                seat.getHoldExpiry().isAfter(LocalDateTime.now())) {
+
+            long seconds = Duration.between(
+                    LocalDateTime.now(), seat.getHoldExpiry()).getSeconds();
+
+            throw new SeatLockedException("Seat locked for " + seconds + " seconds");
+        }
+
+        seat.setStatus(SeatStatus.HELD);
+        seat.setHeldByUserId(userId);
+        seat.setHoldExpiry(LocalDateTime.now().plusMinutes(10));
+
+        return seatRepo.save(seat);
+    }
+}
