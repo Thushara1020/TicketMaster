27380cba33cf8a94f413d0b41431e5a071aacff4
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-20 09:43:52 +0530

    Initial commit

---
+++ TicketMaster/src/test/java/com/ticketmaster/TicketMaster/TicketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketmaster.TicketMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
