fdb4193cdf2463bf63281b432d2b4b18804ffe25
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-20 16:40:56 +0530

    implement optimistic locking and enhance entity relationships

--- TicketMaster/src/main/java/com/ticketmaster/TicketMaster/entity/Seat.java
+++ TicketMaster/src/main/java/com/ticketmaster/TicketMaster/entity/Seat.java
@@ -1,14 +1,13 @@
 package com.ticketmaster.TicketMaster.entity;
 
 import com.ticketmaster.TicketMaster.enums.SeatStatus;
-import jakarta.persistence.*; // මෙතනින් සියලුම JPA annotations (Id, GeneratedValue ඇතුළුව) ලැබෙනවා
+import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 public class Seat {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,4 +23,7 @@ public class Seat {
     @ManyToOne
     @JoinColumn(name = "event_id")
     private Event event;
+
+    @Version
+    private Integer version;
 }
