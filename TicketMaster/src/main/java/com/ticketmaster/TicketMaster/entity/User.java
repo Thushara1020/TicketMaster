af3c895f47ebf60c66fdb2195acc82368b14e908
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-20 13:57:01 +0530

    add User entity and tier mapping

---
+++ TicketMaster/src/main/java/com/ticketmaster/TicketMaster/entity/User.java
@@ -0,0 +1,22 @@
+package com.ticketmaster.TicketMaster.entity;
+
+import com.ticketmaster.TicketMaster.enums.Tier;
+import jakarta.persistence.*; 
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Tier tier;
+
+    private String email;
+
+}
