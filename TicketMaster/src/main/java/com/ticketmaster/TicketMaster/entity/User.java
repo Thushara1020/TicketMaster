1c46c10a3d344bce576f795d9293d64d3bb1143c
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-20 16:39:59 +0530

    implement User-Booking relationship and enhance Seat management

--- TicketMaster/src/main/java/com/ticketmaster/TicketMaster/entity/User.java
+++ TicketMaster/src/main/java/com/ticketmaster/TicketMaster/entity/User.java
@@ -3,20 +3,22 @@ package com.ticketmaster.TicketMaster.entity;
 import com.ticketmaster.TicketMaster.enums.Tier;
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.List;
 
 @Entity
+@Table(name = "app_user")
 @Data
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+    private String email;
 
     @Enumerated(EnumType.STRING)
     private Tier tier;
 
-    private String email;
-
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
 }
