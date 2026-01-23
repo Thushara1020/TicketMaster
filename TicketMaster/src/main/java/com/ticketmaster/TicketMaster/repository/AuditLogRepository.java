39a2ac4edbda7a27a4c21cc50261a7f5bfb067d1
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-23 11:27:30 +0530

    add @Repository annotation to AuditLogRepository

--- TicketMaster/src/main/java/com/ticketmaster/TicketMaster/repository/AuditLogRepository.java
+++ TicketMaster/src/main/java/com/ticketmaster/TicketMaster/repository/AuditLogRepository.java
@@ -1,6 +1,10 @@
 package com.ticketmaster.TicketMaster.repository;
 
+
 import com.ticketmaster.TicketMaster.entity.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
