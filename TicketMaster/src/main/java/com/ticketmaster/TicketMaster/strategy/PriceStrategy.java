a40a732cc116e369fb2aff36bf57ac03644e0e99
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-24 21:20:12 +0530

    remove PriceStrategy

--- TicketMaster/src/main/java/com/ticketmaster/TicketMaster/strategy/PriceStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.ticketmaster.TicketMaster.strategy;
-
-import com.ticketmaster.TicketMaster.dto.PriceResponse;
-import com.ticketmaster.TicketMaster.entity.Event;
-import com.ticketmaster.TicketMaster.entity.User;
-
-public interface PriceStrategy {
-    PriceResponse calculate(User user, Event event);
-}
